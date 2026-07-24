c3b741820b7019a2948750de3587d9ba27f7cffd
khadija0219 <khadijathiam447@gmail.com>
2026-07-24 01:00:34 +0000

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import socket
+import os
+
+app = Flask(__name__)
+
+NOM = "Khadija"
+PRENOM_NOM = "Khadidjiatou_THIAM"  # personnalise avec ton vrai nom si besoin
+VERSION = os.getenv("APP_VERSION", "v1")
+
+@app.route("/")
+def home():
+    hostname = socket.gethostname()
+    return f"""
+    <html>
+        <head><title>Projet GitOps - {khadidjiatou_THIAM}</title></head>
+        <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+            <h1>Bonjour, je suis {khadidjatou} 👋</h1>
+            <h2>Plateforme GitOps observable sur Kubernetes</h2>
+            <p>Version de l'application : <b>{VERSION}</b></p>
+            <p>Pod : <code>{hostname}</code></p>
+        </body>
+    </html>
+    """
+
+@app.route("/health")
+def health():
+    return {"status": "ok", "user": NOM}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
